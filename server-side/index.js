fac1c4c9b328f8e59b56b48a6bad9c31ad6842ad
Nadhif Rafifaiz K <nadhifrafifaiz@gmail.com>
2023-04-03 20:01:05 +0700

    validator

--- server-side/index.js
+++ server-side/index.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const PORT = 8001
 const app = express()
-const {db} = require("./database")
+const { db } = require("./database")
 const cors = require('cors')
-const {authRoutes} = require('./routes')
+const { authRoutes } = require('./routes')
+const { body, validationResult } = require('express-validator')
 
 app.use(cors())
 
 app.use(express.json())
 
-// app.get('/user', async (req, res) => {
-//     let fetchQuery = 'SELECT * FROM users'
-//     db.query(fetchQuery, (err, result) => {
-//         return res.status(200).send(result)
-//     })
-// })
+
+app.post('/validation', body("email").isEmail(), body('password').isLength({ min: 5 }), async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() })
+    }
+    res.status(200).send(req.body)
+})
+
+
+
+
 
 app.use('/auth', authRoutes)
 
